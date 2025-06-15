854f48e911b2108dce2a0b0becc78b564712f066
Pedro Henrique <peedrohs.mesquita@outlook.com>
2025-06-14 22:34:40 -0300

    Biblioteca com funções de desenhar na tela.

---
+++ render.cpp
@@ -0,0 +1,47 @@
+#include "render.h"
+
+void escurecerCor(const Color& corOriginal, Color& cor, float fatorEscurecer) {
+    cor.r = corOriginal.r * fatorEscurecer;
+    cor.g = corOriginal.g * fatorEscurecer;
+    cor.b = corOriginal.b * fatorEscurecer;
+    cor.a = corOriginal.a;
+}
+
+void desenharLetraContorno(const Texto& texto, float grossura) {
+    Color cor;
+    if (texto.mouseSobre) cor = texto.corSobre;
+    else cor = texto.cor;
+    if (grossura > 0.0f) {
+        Color corEscurecida;
+        escurecerCor(cor, corEscurecida, 0.70f);
+        DrawTextEx(texto.fonte, texto.conteudo,
+                   (Vector2){texto.posicao.x - grossura, texto.posicao.y - grossura},
+                   texto.tamanho, texto.espacamento, corEscurecida);
+        DrawTextEx(texto.fonte, texto.conteudo,
+                   (Vector2){texto.posicao.x + grossura, texto.posicao.y - grossura},
+                   texto.tamanho, texto.espacamento, corEscurecida);
+        DrawTextEx(texto.fonte, texto.conteudo,
+                   (Vector2){texto.posicao.x - grossura, texto.posicao.y + grossura},
+                   texto.tamanho, texto.espacamento, corEscurecida);
+        DrawTextEx(texto.fonte, texto.conteudo,
+                   (Vector2){texto.posicao.x + grossura, texto.posicao.y + grossura},
+                   texto.tamanho, texto.espacamento, corEscurecida);
+    }
+    DrawTextEx(texto.fonte, texto.conteudo, texto.posicao, texto.tamanho, texto.espacamento, cor);
+
+}
+
+void desenharBotao(const CaixaTexto &ct, float grossura, float escurecer) {
+    Color corEscurecida, cor;
+
+    if (ct.caixa.mouseSobre)
+        cor = ct.caixa.corSobre;
+    else cor = ct.caixa.cor;
+
+    escurecerCor(cor, corEscurecida, escurecer);
+    DrawRectangleRounded(ct.caixa.retangulo, ct.caixa.redondeza, ct.caixa.segmentos, cor);
+    DrawRectangleRoundedLines(ct.caixa.retangulo, ct.caixa.redondeza, ct.caixa.segmentos, corEscurecida);
+    desenharLetraContorno(ct.texto, grossura);
+}
+
+
---
+++ render.h
@@ -0,0 +1,9 @@
+#ifndef RENDER_H
+#define RENDER_H
+
+#include "raylib.h"
+#include "interface.h"
+
+void desenharBotao(const CaixaTexto &ct, float grossura, float escurecer);
+
+#endif // RENDER_H
