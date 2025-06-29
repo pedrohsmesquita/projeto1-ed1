507c449021d4aaaa8761ffea810245854dc106d9
Pedro Henrique <peedrohs.mesquita@outlook.com>
2025-06-29 17:26:34 -0300

    Estrutura de controle

--- teclado.h
+++ teclado.h
@@ -6,20 +6,30 @@
 #include "interface.h"
 #include "string_lista.h"
 
-void lerEntradaTeclado(string_lista::String &listaS, lista_grafica::ListaLetra &listaV, int maxTam) {
+struct TecladoStatus {
+    bool insercao;
+    bool exclusao;
+};
+
+TecladoStatus lerEntradaTeclado(string_lista::String &listaS, lista_grafica::ListaLetra &listaV, int maxTam) {
     int tecla = toupper(GetCharPressed());
+    TecladoStatus status = {false, false};
 
     while (tecla > 0) {
         if (tecla >= 'A' && tecla <= 'Z' && listaS.primeiro->val < maxTam) {
             string_lista::insereFinal(listaS, tecla);
-            lista_grafica::insereFinal(listaV, listaS.ultimo);
+            lista_grafica::utils::inserirLetra(listaV, listaS.ultimo, true, 2.0f);
+            status.insercao = true;
         }
         tecla = toupper(GetCharPressed());
     }
     if (IsKeyPressed(KEY_BACKSPACE)) {
         string_lista::removeFinal(listaS);
         lista_grafica::removeFinal(listaV);
+        status.exclusao = true;
     }
+
+    return status;
 }
 
 
